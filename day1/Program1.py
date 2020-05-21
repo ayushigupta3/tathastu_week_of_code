81a9fb01ce4ebefc0731fa07cea0b34251ee0345
Ayushi Gupta <65649679+ayushigupta3@users.noreply.github.com>
2020-05-21 15:30:48 +0530

    Create Program1.py

---
+++ day1/Program1.py
@@ -0,0 +1,18 @@
+Name = input("enter your name")
+Branch = input("enter your branch")
+Gender = input("enter your gender")
+College = input("enter your college name")
+Age = input("enter your age")
+
+print(Name +"\n"+Branch +"\n"+Gender+"\n"+ College+"\n"+Age)
+
+
+
+
+
+
+
+
+
+
+
